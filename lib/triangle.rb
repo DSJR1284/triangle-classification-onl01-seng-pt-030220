0a58e0f1bd96fd8bb1c66eb07651ad116fdd968c
Doug Jones <dsjr1284@gmail.com>
2020-04-29 00:46:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,13 +10,13 @@ class Triangle
 
   def kind()
     if @side_a > @side_b && @side_a > @side_c
-      "scalene"
+      scalene
     elsif
       @side_a == @side_b && @side_a > @side_c
-      "isosceles"
+      isosceles
     else
       @side_a == @side_b && @side_a == @side_c
-      "equliateral"
+      equliateral
     end
 
   end
