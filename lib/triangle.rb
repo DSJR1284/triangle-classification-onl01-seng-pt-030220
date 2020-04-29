bf66ac4dd3b76e11cbbe4fa07cd0b88cb329ee64
Doug Jones <dsjr1284@gmail.com>
2020-04-29 00:18:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_a, :side_b, :side_c
+
+  def initializae(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind()
+    if side_a > side_b && side_a > side_c
+      puts scalene triangle
+    elsif
+      side_a == side_b && > side_c
+      puts isosceles
+    else
+      side_a && side_b == side_c
+      puts equliateral
+    end
+
+  end
+
+
+
+
+
+  class TriangleError < StandardError
+
+  end
+
 end
